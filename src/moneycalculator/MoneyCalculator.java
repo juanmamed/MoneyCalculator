f9536a97555b5fbb69a0686d2d666f2aaca5402f
juanmamed <juanmamr21@gmail.com>
2022-01-04 12:04:10 +0000

    MoneyCalculator version final 2.0

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -15,17 +15,9 @@ public class MoneyCalculator {
         String file = "currencies.txt";
         String url_base = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/";
         List<Currency> divisas = CurrencyLoader.loadCurrencies(file);
-        
-        //for(int i = 0; i < divisas.size(); i++) {
-        //    System.out.println(divisas.get(i));
-        //}
-        
+
         List<ExchangeRate> exchangeRates = ExchangeRateLoader.loadExchangeRates(url_base, divisas);
-        
-        //for(int i = 0; i < exchangeRates.size(); i++) {
-        //    System.out.println(exchangeRates.get(i).getFrom() + " to " + exchangeRates.get(i).getTo() + " rate: " + exchangeRates.get(i).getRate());
-        //}
-        
+
         MainFrame gui = new MainFrame(divisas, exchangeRates);
     }
 }
